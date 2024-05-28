190b32c6b015413c1084bb3434480c9823a33658
Muhammad Adam Majid <117140539+notRobot200@users.noreply.github.com>
2024-05-28 21:37:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def predict():
     return jsonify({'predicted_label': predicted_label, 'matching_entries': matching_entries.to_dict(orient='records')})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
