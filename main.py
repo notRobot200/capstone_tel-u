53ae9f7f44cd891fff71d7c8fa2513f686346a16
Muhammad Adam Majid <117140539+notRobot200@users.noreply.github.com>
2024-07-09 15:35:08 +0700

    Update main.py
    fix function tarif bpjs

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask import Flask, request, jsonify
 import joblib
 import os
 from flask_cors import CORS  # Import CORS
+from googletrans import Translator
+import spacy
+import numpy as np
 
 # Inisialisasi Flask
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 
-# Fungsi untuk melakukan koneksi ke MySQL dan mendapatkan data dari tabel 'data_icd'
 def get_data_icd_from_db():
     try:
         db_connection = mysql.connector.connect(
@@ -19,8 +21,8 @@ def get_data_icd_from_db():
             database="oetomo"
         )
 
-        # Query untuk mengambil semua data dari tabel 'data_icd'
-        query = "SELECT * FROM data_icd"
+        # Query untuk mengambil semua data dari tabel 'data_icd_en'
+        query = "SELECT * FROM data_icd_en"
 
         # Eksekusi query dan simpan hasilnya dalam DataFrame
         df_icd = pd.read_sql(query, con=db_connection)
@@ -33,7 +35,6 @@ def get_data_icd_from_db():
     except mysql.connector.Error as err:
         print(f"Error while connecting to MySQL: {err}")
         return pd.DataFrame()  # Mengembalikan DataFrame kosong jika terjadi kesalahan
-
 
 # Fungsi untuk mengambil hasil pencarian dari tabel 'search_icd'
 def get_search_icd_results_from_db():
@@ -68,52 +69,52 @@ def predict_label_disease(model, tfidf_vectorizer, disease_name):
     return predicted_label[0]
 
 
-# Fungsi untuk mencari kandidat ICD berdasarkan query
-def search_icd_candidates(query, dataset, model, tfidf_vectorizer):
-    # Predict ICD category using the trained model
-    predicted_label = predict_label_disease(model, tfidf_vectorizer, query)
-
-    # Mapping of predicted labels to chapter ranges or codes
-    chapter_mapping = {
-        'Chapter I': 'A00-B99',
-        'Chapter II': 'C00-D48',
-        'Chapter III': 'D50-D89',
-        'Chapter IV': 'E00-E90',
-        'Chapter V': 'F00-F99',
-        'Chapter VI': 'G00-G99',
-        'Chapter VII': 'H00-H59',
-        'Chapter VIII': 'H60-H95',
-        'Chapter IX': 'I00-I99',
-        'Chapter X': 'J00-J99',
-        'Chapter XI': 'K00-K93',
-        'Chapter XII': 'L00-L99',
-        'Chapter XIII': 'M00-M99',
-        'Chapter XIV': 'N00-N99',
-        'Chapter XV': 'O00-O99',
-        'Chapter XVI': 'P00-P96',
-        'Chapter XVII': 'Q00-Q99',
-        'Chapter XVIII': 'R00-R99',
-        'Chapter XIX': 'S00-T98',
-        'Chapter XX': 'V01-Y98',
-        'Chapter XXI': 'Z00-Z99',
-        'Chapter XXII': 'U00-U99'
-    }
-
-    # Get the range for the predicted chapter
-    chapter_range = chapter_mapping.get(predicted_label)
-
-    if not chapter_range:
-        # If the predicted label is not in the mapping, return an empty DataFrame
-        return pd.DataFrame()
-
-    # Filter dataset based on the predicted chapter
-    start_code, end_code = chapter_range.split('-')
-    filtered_dataset = dataset[(dataset['kode_icd'] >= start_code) & (dataset['kode_icd'] <= end_code)]
-
-    # Search for matching entries based on the query
-    matching_entries = filtered_dataset[filtered_dataset['nama_penyakit'].str.contains(query, case=False)]
-
-    return matching_entries
+# # Fungsi untuk mencari kandidat ICD berdasarkan query
+# def search_icd_candidates(query, dataset, model, tfidf_vectorizer):
+#     # Predict ICD category using the trained model
+#     predicted_label = predict_label_disease(model, tfidf_vectorizer, query)
+#
+#     # Mapping of predicted labels to chapter ranges or codes
+#     chapter_mapping = {
+#         'Chapter I': 'A00-B99',
+#         'Chapter II': 'C00-D48',
+#         'Chapter III': 'D50-D89',
+#         'Chapter IV': 'E00-E90',
+#         'Chapter V': 'F00-F99',
+#         'Chapter VI': 'G00-G99',
+#         'Chapter VII': 'H00-H59',
+#         'Chapter VIII': 'H60-H95',
+#         'Chapter IX': 'I00-I99',
+#         'Chapter X': 'J00-J99',
+#         'Chapter XI': 'K00-K93',
+#         'Chapter XII': 'L00-L99',
+#         'Chapter XIII': 'M00-M99',
+#         'Chapter XIV': 'N00-N99',
+#         'Chapter XV': 'O00-O99',
+#         'Chapter XVI': 'P00-P96',
+#         'Chapter XVII': 'Q00-Q99',
+#         'Chapter XVIII': 'R00-R99',
+#         'Chapter XIX': 'S00-T98',
+#         'Chapter XX': 'V01-Y98',
+#         'Chapter XXI': 'Z00-Z99',
+#         'Chapter XXII': 'U00-U99'
+#     }
+#
+#     # Get the range for the predicted chapter
+#     chapter_range = chapter_mapping.get(predicted_label)
+#
+#     if not chapter_range:
+#         # If the predicted label is not in the mapping, return an empty DataFrame
+#         return pd.DataFrame()
+#
+#     # Filter dataset based on the predicted chapter
+#     start_code, end_code = chapter_range.split('-')
+#     filtered_dataset = dataset[(dataset['kode_icd'] >= start_code) & (dataset['kode_icd'] <= end_code)]
+#
+#     # Search for matching entries based on the query
+#     matching_entries = filtered_dataset[filtered_dataset['nama_penyakit'].str.contains(query, case=False)]
+#
+#     return matching_entries
 
 
 # Fungsi untuk menghapus data dari tabel 'search_icd'
@@ -224,6 +225,47 @@ def insert_query_to_db(query):
         print(f"Error while inserting to MySQL: {err}")
 
 #MASIH PENGEMBANGAN
+# # Database connection
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="34.145.29.172",
+#         user="beingman",
+#         password="123",
+#         database="oetomo"
+#     )
+#
+# # Function to fetch data from a table
+# def fetch_data_from_table(table_name):
+#     db_connection = get_db_connection()
+#     query = f"SELECT * FROM `{table_name}`"
+#     df = pd.read_sql(query, db_connection)
+#     db_connection.close()
+#     return df
+#
+# # Function to fetch the disease name from query_disease table
+# def fetch_disease_name():
+#     db_connection = get_db_connection()
+#     query = "SELECT query FROM query_disease ORDER BY id DESC LIMIT 1;"  # Adjust query as needed
+#     cursor = db_connection.cursor()
+#     cursor.execute(query)
+#     result = cursor.fetchone()
+#     db_connection.close()
+#     return result[0] if result else None
+#
+# # Search functions
+# def search_disease(dataset, keywords, sort_column='TARIF KELAS 1', ascending=False):
+#     if 'TARIF KELAS' in sort_column:
+#         matching_entries = dataset[dataset['DESKRIPSI KODE INA-CBG'].str.contains(keywords, case=False)]
+#     else:
+#         matching_entries = dataset[dataset['DESKRIPSI KODE INA-CBG'].str.contains(keywords, case=False)].sort_values(by=sort_column, ascending=ascending)
+#     return matching_entries
+#
+# def search_disease_keywords(dataset, keywords, sort_column='TARIF KELAS 1', ascending=False):
+#     all_matches = pd.DataFrame()
+#     for key in keywords:
+#         matches = dataset[dataset['DESKRIPSI KODE INA-CBG'].str.contains(key, case=False)]
+#         all_matches = pd.concat([all_matches, matches])
+#     return all_matches.sort_values(by=sort_column, ascending=ascending)
 # Database connection
 def get_db_connection():
     return mysql.connector.connect(
@@ -244,7 +286,7 @@ def fetch_data_from_table(table_name):
 # Function to fetch the disease name from query_disease table
 def fetch_disease_name():
     db_connection = get_db_connection()
-    query = "SELECT query FROM query_disease ORDER BY id DESC LIMIT 1;"  # Adjust query as needed
+    query = "SELECT query FROM query_disease ORDER BY id DESC LIMIT 1;"
     cursor = db_connection.cursor()
     cursor.execute(query)
     result = cursor.fetchone()
@@ -265,11 +307,48 @@ def search_disease_keywords(dataset, keywords, sort_column='TARIF KELAS 1', asce
         matches = dataset[dataset['DESKRIPSI KODE INA-CBG'].str.contains(key, case=False)]
         all_matches = pd.concat([all_matches, matches])
     return all_matches.sort_values(by=sort_column, ascending=ascending)
-#MASIH PENGEMBANGAN
+# #MASIH PENGEMBANGAN
+
+# Fungsi untuk menerjemahkan teks ke bahasa Inggris
+def translate_to_english(text):
+    translator = Translator()
+    translation = translator.translate(text, src='id', dest='en')
+    return translation.text
+
+# Fungsi untuk praproses teks
+def preprocess_text(text):
+    doc = nlp(text)
+    return ' '.join([token.lemma_ for token in doc if not token.is_stop])
+
+# Function to convert text to vector
+def text_to_vector(text):
+    return nlp(text).vector
+
+# Fungsi untuk mencari kandidat ICD menggunakan NLP
+def search_icd_candidates(query, dataset):
+    # Praproses query
+    query_processed = preprocess_text(query)
+    query_vector = nlp(query_processed).vector
+
+    # Calculate similarity scores
+    def cosine_similarity(vec1, vec2):
+        dot_product = np.dot(vec1, vec2)
+        norm_a = np.linalg.norm(vec1)
+        norm_b = np.linalg.norm(vec2)
+        return dot_product / (norm_a * norm_b)
+
+    dataset['similarity'] = dataset['Nama_Penyakit_Vector'].apply(lambda x: cosine_similarity(x, query_vector))
+
+    # Urutkan berdasarkan skor kemiripan dan kembalikan hasil teratas
+    matching_entries = dataset.sort_values(by='similarity', ascending=False).head(10)
+
+    return matching_entries
 
 # Load model dan TF-IDF vectorizer
-model = joblib.load('model_svc.pkl')
-tfidf_vectorizer = joblib.load('tfidf_vectorizer.pkl')
+model = joblib.load('model_svc_en.pkl')
+tfidf_vectorizer = joblib.load('tfidf_vectorizer_en.pkl')
+# Memuat model bahasa spaCy yang besar
+nlp = spacy.load('en_core_web_lg')
 
 @app.route("/")
 def main():
@@ -288,29 +367,43 @@ def search_icd():
 
     # Ambil query dari data JSON
     query = request_data['query']
+    use_nlp = request_data.get('use_nlp', False)  # Default to False if not provided
 
-    # Sisipkan query ke dalam tabel 'query_disease'
-    insert_query_to_db(query)
+    # Terjemahkan query ke bahasa Inggris
+    query_in_english = translate_to_english(query)
 
-    # Panggil fungsi get_data_icd_from_db untuk mendapatkan data dari database
-    df_icd = get_data_icd_from_db()
-
-    # Panggil fungsi search_icd_candidates dengan query yang diberikan
-    matching_entries = search_icd_candidates(query, df_icd, model, tfidf_vectorizer)
-
-    # Hapus data lama dari tabel 'search_icd'
-    clear_search_icd_table()
-
-    # Format hasil pencarian sebagai JSON
-    if not matching_entries.empty:
-        result = matching_entries[['kode_icd', 'nama_penyakit']].to_dict(orient='records')
-
-        # Sisipkan hasil pencarian ke tabel 'search_icd'
-        insert_search_results_to_db(result)
-
-        return jsonify(result)
+    if use_nlp:
+        # Panggil fungsi search_icd_candidates dengan query yang diberikan
+        matching_entries = search_icd_candidates(query_in_english, get_data_icd_from_db())
+        # Format hasil pencarian sebagai JSON
+        if not matching_entries.empty:
+            result = matching_entries[['kode_icd', 'nama_penyakit', 'similarity']].to_dict(orient='records')
+            return jsonify(result)
+        else:
+            return jsonify({'message': f'No matching ICD codes found for query: {query_in_english}'})
     else:
-        return jsonify({'message': f'No matching ICD codes found for query: {query}'})
+        # Sisipkan query ke dalam tabel 'query_disease'
+        insert_query_to_db(query)
+
+        # Panggil fungsi get_data_icd_from_db untuk mendapatkan data dari database
+        df_icd_manual = get_data_icd_from_db()
+
+        # Hapus data lama dari tabel 'search_icd'
+        clear_search_icd_table()
+
+        # Filter data ICD berdasarkan query manual
+        matching_entries = df_icd_manual[df_icd_manual['nama_penyakit'].str.contains(query_in_english, case=False, na=False)]
+
+        # Format hasil pencarian sebagai JSON
+        if not matching_entries.empty:
+            result = matching_entries[['kode_icd', 'nama_penyakit']].to_dict(orient='records')
+
+            # Sisipkan hasil pencarian ke tabel 'search_icd'
+            insert_search_results_to_db(result)
+
+            return jsonify(result)
+        else:
+            return jsonify({'message': f'No matching ICD codes found for query: {query}'})
 
 
 @app.route('/select_icd', methods=['POST'])
@@ -349,6 +442,65 @@ def select_icd():
     })
 
 #MASIH PENGEMBANGAN
+# # API endpoint for searching diseases
+# @app.route('/search_disease', methods=['GET'])
+# def search_disease_api():
+#     # Input parameters
+#     jenis_pelayanan = request.args.get('jenis_pelayanan', default='inap').strip().lower()
+#
+#     # Fetch disease name from the database
+#     disease_name = fetch_disease_name()
+#     if not disease_name:
+#         return jsonify({"error": "Tidak ada nama penyakit yang ditemukan di database."}), 400
+#
+#     # Selecting dataset based on input
+#     if jenis_pelayanan == 'inap':
+#         dataset = fetch_data_from_table('data_ina-cbg')
+#     elif jenis_pelayanan == 'jalan':
+#         dataset = fetch_data_from_table('df_ina_rj')
+#     else:
+#         return jsonify({"error": "Jenis pelayanan yang dimasukkan tidak valid."}), 400
+#
+#     # Sorting column based on input
+#     sort_column_input = request.args.get('sort_column', default='TARIF KELAS 1').strip()
+#     ascending_input = request.args.get('ascending', default='n').strip().lower()
+#     ascending = ascending_input == 'y' if ascending_input in ['y', 'n'] else False
+#
+#     if dataset is fetch_data_from_table('df_ina_rj'):
+#         sort_column = 'TARIF INA-CBG'
+#     else:
+#         if sort_column_input.isdigit():
+#             if sort_column_input in ['1', '2', '3']:
+#                 sort_column = 'TARIF KELAS ' + sort_column_input
+#             else:
+#                 return jsonify({"error": "Nomor kolom tarif kelas harus '1', '2', atau '3'."}), 400
+#         elif sort_column_input.startswith('TARIF KELAS') and sort_column_input.split()[2].isdigit():
+#             sort_column = sort_column_input
+#         else:
+#             sort_column = 'TARIF KELAS 1'
+#
+#     # Searching disease in the selected dataset
+#     matching_entries = search_disease(dataset, disease_name, sort_column=sort_column, ascending=ascending)
+#
+#     if matching_entries.empty:
+#         # If no results match, search based on keywords
+#         keywords = disease_name.split()
+#         matching_entries = search_disease_keywords(dataset, keywords, sort_column=sort_column, ascending=ascending)
+#
+#     # Formatting the results
+#     if not matching_entries.empty:
+#         if dataset is fetch_data_from_table('df_ina_rj'):
+#             result = matching_entries[['KODE INA-CBG', 'DESKRIPSI KODE INA-CBG', 'TARIF INA-CBG']].head(10).to_dict(orient='records')
+#         else:
+#             tarif_columns = [col for col in matching_entries.columns if 'TARIF KELAS' in col]
+#             if tarif_columns:
+#                 result = matching_entries[['KODE INA-CBG', 'DESKRIPSI KODE INA-CBG', sort_column]].head(10).to_dict(orient='records')
+#             else:
+#                 result = "Tidak ada kolom tarif kelas yang cocok untuk ditampilkan."
+#     else:
+#         result = "Tidak ada hasil yang cocok."
+#
+#     return jsonify(result)
 # API endpoint for searching diseases
 @app.route('/search_disease', methods=['GET'])
 def search_disease_api():
@@ -363,17 +515,19 @@ def search_disease_api():
     # Selecting dataset based on input
     if jenis_pelayanan == 'inap':
         dataset = fetch_data_from_table('data_ina-cbg')
+        default_sort_column = 'TARIF KELAS 1'
     elif jenis_pelayanan == 'jalan':
         dataset = fetch_data_from_table('df_ina_rj')
+        default_sort_column = 'TARIF INA-CBG'
     else:
         return jsonify({"error": "Jenis pelayanan yang dimasukkan tidak valid."}), 400
 
     # Sorting column based on input
-    sort_column_input = request.args.get('sort_column', default='TARIF KELAS 1').strip()
+    sort_column_input = request.args.get('sort_column', default=default_sort_column).strip()
     ascending_input = request.args.get('ascending', default='n').strip().lower()
     ascending = ascending_input == 'y' if ascending_input in ['y', 'n'] else False
 
-    if dataset is fetch_data_from_table('df_ina_rj'):
+    if jenis_pelayanan == 'jalan':
         sort_column = 'TARIF INA-CBG'
     else:
         if sort_column_input.isdigit():
@@ -384,7 +538,7 @@ def search_disease_api():
         elif sort_column_input.startswith('TARIF KELAS') and sort_column_input.split()[2].isdigit():
             sort_column = sort_column_input
         else:
-            sort_column = 'TARIF KELAS 1'
+            sort_column = default_sort_column
 
     # Searching disease in the selected dataset
     matching_entries = search_disease(dataset, disease_name, sort_column=sort_column, ascending=ascending)
@@ -396,7 +550,7 @@ def search_disease_api():
 
     # Formatting the results
     if not matching_entries.empty:
-        if dataset is fetch_data_from_table('df_ina_rj'):
+        if jenis_pelayanan == 'jalan':
             result = matching_entries[['KODE INA-CBG', 'DESKRIPSI KODE INA-CBG', 'TARIF INA-CBG']].head(10).to_dict(orient='records')
         else:
             tarif_columns = [col for col in matching_entries.columns if 'TARIF KELAS' in col]
